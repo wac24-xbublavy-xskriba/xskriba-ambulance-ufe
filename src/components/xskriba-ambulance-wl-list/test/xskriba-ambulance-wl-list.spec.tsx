0bf7c7968425a342b9162ebb0ce58833fcbfcaef
Peter Škríba <peter.skriba@gmail.com>
2024-02-13 12:18:17 +0100

    feat(c1): unit tests

--- src/components/xskriba-ambulance-wl-list/test/xskriba-ambulance-wl-list.spec.tsx
+++ src/components/xskriba-ambulance-wl-list/test/xskriba-ambulance-wl-list.spec.tsx
@@ -1,18 +1,17 @@
-import { newSpecPage } from '@stencil/core/testing';
-import { XskribaAmbulanceWlList } from '../xskriba-ambulance-wl-list';
+import { newSpecPage } from '@stencil/core/testing'
+import { XskribaAmbulanceWlList } from '../xskriba-ambulance-wl-list'
 
 describe('xskriba-ambulance-wl-list', () => {
   it('renders', async () => {
     const page = await newSpecPage({
       components: [XskribaAmbulanceWlList],
-      html: `<xskriba-ambulance-wl-list></xskriba-ambulance-wl-list>`,
-    });
-    expect(page.root).toEqualHtml(`
-      <xskriba-ambulance-wl-list>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </xskriba-ambulance-wl-list>
-    `);
-  });
-});
+      html: `<xskriba-ambulance-wl-list></xskriba-ambulance-wl-list>`
+    })
+
+    const wlList = page.rootInstance as XskribaAmbulanceWlList
+    const expectedPatients = wlList?.waitingPatients?.length
+
+    const items = page.root.shadowRoot.querySelectorAll('md-list-item')
+    expect(items.length).toEqual(expectedPatients)
+  })
+})
--- stencil.config.ts
+++ stencil.config.ts
@@ -2,6 +2,7 @@ import { Config } from '@stencil/core'
 
 export const config: Config = {
   namespace: 'ambulance-ufe',
+  globalScript: 'src/global/app.ts',
   outputTargets: [
     {
       type: 'dist',
